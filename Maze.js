02c25815ead40ba1e20c75900b12442bfb21d66d
brandonpayton <brandon@happycode.net>
2012-07-19 00:28:13 -0700

    Added some navigation-related helpers to Maze interface.

--- Maze.js
+++ Maze.js
@@ -3,88 +3,107 @@ define([
 ], function(declare) {
     return declare(null, {
 
-        canGoUp: function(row, column) {
-            return row > 0 && !this._cells[row - 1][column].bottom
-        }
-        , canGoDown: function(row, column) {
-            return row < this._cells.length && !this._cells[row][column].bottom
-        }
-        , canGoLeft: function(row, column) {
-            return column > 0 && !this._cells[row][column - 1].right
-        }
-        , canGoRight: function(row, column) {
-            return column < this._cells[row].length && !this._cells[row][column].right
-        }
-        , constructor: function(args) {
+        constructor: function(args) {
             var high = this.high = args.high
             var wide = this.wide = args.wide
             var cells = this._cells = []
+            var touchedCells = []
             
             var i, j
             for(i = 0; i < high; i++) {
                 cells[i] = []
+                touchedCells[i] = []
                 for(j = 0; j < wide; j++) {
-                    cells[i][j] = { right: true, bottom: true }
+                    cells[i][j] = { rightWall: true, bottomWall: true }
                 }
             }
             
-            function getNextCell(row, column) {
-                var options = [];
-                // Top
-                if(row > 0 && !cells[row - 1][column].touched) {
-                    options.push({
-                        visit: function() {
-                            cells[row - 1][column].bottom = false
-                            cells[row - 1][column].touched = true
-                            return { row: row - 1, column: column }
-                        }
-                    })
+            var self = this
+            function moveToNextCell(row, column) {
+                var inaccessibleNeighbors = self.getInaccessibleNeighbors(row, column)
+                var options = inaccessibleNeighbors.reduce(function(untouchedNeighbors, neighbor) {
+                    if(!touchedCells[neighbor.row][neighbor.column]) untouchedNeighbors.push(neighbor)
+                    return untouchedNeighbors
+                }, [])
+
+                if(options.length === 0) {
+                    return null
+                } else {
+                    var chosenCell = options[Math.floor(Math.random() * options.length)]
+                    touchedCells[chosenCell.row][chosenCell.column] = true
+
+                    // Remove wall
+                    if(chosenCell.row < row) {
+                        cells[chosenCell.row][chosenCell.column].bottomWall = false
+                    } else if(chosenCell.row > row) {
+                        cells[row][column].bottomWall = false
+                    } else if(chosenCell.column < column) {
+                        cells[chosenCell.row][chosenCell.column].rightWall = false
+                    } else if(chosenCell.column > column) {
+                        cells[row][column].rightWall = false
+                    } else {
+                        throw new Error("Error moving to next cell during initialization.")
+                    }
+
+                    return chosenCell
                 }
-                // Bottom
-                if(row < (cells.length - 1) && !cells[row + 1][column].touched) {
-                    options.push({
-                        visit: function() {
-                            cells[row][column].bottom = false
-                            cells[row + 1][column].touched = true
-                            return { row: row + 1, column: column }
-                        }
-                    })
-                }
-                // Left
-                if(column > 0 && !cells[row][column - 1].touched) {
-                    options.push({
-                        visit: function() {
-                            cells[row][column - 1].right = false
-                            cells[row][column - 1].touched = true
-                            return { row: row, column: column - 1 }
-                        }
-                    });
-                }
-                // Right
-                if(column < (cells[row].length - 1) && !cells[row][column + 1].touched) {
-                    options.push({
-                        visit: function() {
-                            cells[row][column].right = false
-                            cells[row][column + 1].touched = true
-                            return { row: row, column: column + 1 }
-                        }
-                    });
-                }
-                
-                return options.length === 0
-                    ? null
-                    : options[Math.floor(Math.random() * options.length)].visit()
             }
 
-            cells[0][0].touched = true
+            touchedCells[0][0] = true
             var stack = [{ row: 0, column: 0 }]
             while(stack.length > 0) {
                 var top = stack[stack.length - 1]
-                var next = getNextCell(top.row, top.column)
+                var next = moveToNextCell(top.row, top.column)
 
                 if(next) stack.push(next)
                 else stack.pop()
             }
         }
-    });
-});
+        , canGoUp: function(row, column) {
+            return row > 0 && !this._cells[row - 1][column].bottomWall
+        }
+        , canGoDown: function(row, column) {
+            return row < this._cells.length && !this._cells[row][column].bottomWall
+        }
+        , canGoLeft: function(row, column) {
+            return column > 0 && !this._cells[row][column - 1].rightWall
+        }
+        , canGoRight: function(row, column) {
+            return column < this._cells[row].length && !this._cells[row][column].rightWall
+        }
+        , getAccessibleNeighbors: function(row, column) {
+            var neighbors = []
+            if(this.canGoUp(row, column)) {
+                neighbors.push({ row: row - 1, column: column })
+            }
+            if(this.canGoDown(row, column)) {
+                neighbors.push({ row: row + 1, column: column })
+            }
+            if(this.canGoLeft(row, column)) {
+                neighbors.push({ row: row, column: column - 1 })
+            }
+            if(this.canGoRight(row, column)) {
+                neighbors.push({ row: row, column: column + 1 })
+            }
+            return neighbors
+        }
+
+        , getInaccessibleNeighbors: function(row, column) {
+            var cells = this._cells
+            var neighbors = []
+            if(!this.canGoUp(row, column) && row > 0) {
+                neighbors.push({ row: row - 1, column: column })
+            }
+            if(!this.canGoDown(row, column) && row < (cells.length - 1)) {
+                neighbors.push({ row: row + 1, column: column })
+            }
+            if(!this.canGoLeft(row, column) && column > 0) {
+                neighbors.push({ row: row, column: column - 1 })
+            }
+            if(!this.canGoRight(row, column) && column < (cells[row].length - 1)) {
+                neighbors.push({ row: row, column: column + 1 })
+            }
+            return neighbors
+        },
+    })
+})
