c7fd6f9bfd829607c65eec4d92fc71e99a62e81b
brandonpayton <brandon@happycode.net>
2012-07-21 17:23:40 -0700

    Moved solving logic into Solver object. Clarified Maze initialization logic.

--- Maze.js
+++ Maze.js
@@ -1,30 +1,34 @@
 define([
-    "dojo/_base/declare"
-], function(declare) {
+    "dojo/_base/declare",
+    "dojo/_base/array"
+], function(declare, arrayUtil) {
     return declare(null, {
-
+        startLocation: { row: 0, column: 0 },
+        endLocation: null,
         constructor: function(args) {
-            var high = this.high = args.high
-            var wide = this.wide = args.wide
+            var numRows = this.numRows = args.numRows
+            var numColumns = this.numColumns = args.numColumns
+
+            this.endLocation = { row: numRows - 1, column: numColumns - 1 };
+
             var cells = this._cells = []
             var touchedCells = []
             
             var i, j
-            for(i = 0; i < high; i++) {
+            for(i = 0; i < numRows; i++) {
                 cells[i] = []
                 touchedCells[i] = []
-                for(j = 0; j < wide; j++) {
+                for(j = 0; j < numColumns; j++) {
                     cells[i][j] = { rightWall: true, bottomWall: true }
                 }
             }
             
             var self = this
-            function moveToNextCell(row, column) {
-                var inaccessibleNeighbors = self.getInaccessibleNeighbors(row, column)
-                var options = inaccessibleNeighbors.reduce(function(untouchedNeighbors, neighbor) {
-                    if(!touchedCells[neighbor.row][neighbor.column]) untouchedNeighbors.push(neighbor)
-                    return untouchedNeighbors
-                }, [])
+            function moveToNextUnvisitedCell(row, column) {
+                var neighbors = self.getNeighbors(row, column)
+                var options = arrayUtil.filter(neighbors, function(neighbor) {
+                    return !touchedCells[neighbor.row][neighbor.column]
+                })
 
                 if(options.length === 0) {
                     return null
@@ -53,7 +57,7 @@ define([
             var stack = [{ row: 0, column: 0 }]
             while(stack.length > 0) {
                 var top = stack[stack.length - 1]
-                var next = moveToNextCell(top.row, top.column)
+                var next = moveToNextUnvisitedCell(top.row, top.column)
 
                 if(next) stack.push(next)
                 else stack.pop()
@@ -71,39 +75,42 @@ define([
         , canGoRight: function(row, column) {
             return column < this._cells[row].length && !this._cells[row][column].rightWall
         }
+
+        , getNeighbors: function(row, column) {
+            var neighbors = [ ];
+
+            // Top
+            if(row > 0) neighbors.push({ row: row - 1, column: column })
+
+            // Left
+            if(column > 0) neighbors.push({ row: row, column: column - 1 })
+
+            // Bottom
+            var lastRow = this._cells.length - 1
+            if(row < lastRow) neighbors.push({ row: row + 1, column: column })
+
+            // Right
+            var lastColumn = this._cells[row].length - 1;
+            if(column < lastColumn) neighbors.push({ row: row, column: column + 1 })
+
+            return neighbors;
+        }
+
         , getAccessibleNeighbors: function(row, column) {
             var neighbors = []
             if(this.canGoUp(row, column)) {
                 neighbors.push({ row: row - 1, column: column })
             }
-            if(this.canGoDown(row, column)) {
-                neighbors.push({ row: row + 1, column: column })
-            }
             if(this.canGoLeft(row, column)) {
                 neighbors.push({ row: row, column: column - 1 })
+            }
+            if(this.canGoDown(row, column)) {
+                neighbors.push({ row: row + 1, column: column })
             }
             if(this.canGoRight(row, column)) {
                 neighbors.push({ row: row, column: column + 1 })
             }
             return neighbors
         }
-
-        , getInaccessibleNeighbors: function(row, column) {
-            var cells = this._cells
-            var neighbors = []
-            if(!this.canGoUp(row, column) && row > 0) {
-                neighbors.push({ row: row - 1, column: column })
-            }
-            if(!this.canGoDown(row, column) && row < (cells.length - 1)) {
-                neighbors.push({ row: row + 1, column: column })
-            }
-            if(!this.canGoLeft(row, column) && column > 0) {
-                neighbors.push({ row: row, column: column - 1 })
-            }
-            if(!this.canGoRight(row, column) && column < (cells[row].length - 1)) {
-                neighbors.push({ row: row, column: column + 1 })
-            }
-            return neighbors
-        },
     })
 })
--- Solver.js
+++ Solver.js
@@ -1,8 +1,52 @@
 define([
     "dojo/_base/declare",
+    "dojo/_base/lang",
     "dojo/Evented"
-], function() {
-    return declare(null, {
-        
+], function(declare, lang, Evented) {
+    return declare([ Evented ], {
+        _maze: null,
+        _cellStack: [ ],
+        _visitedCells: null,
+        constructor: function(maze) {
+            this._maze = maze;
+            this._cellStack.push(maze.startLocation);
+
+            this._visitedCells = [ ];
+            for(var i = 0; i < maze.numRows; i++) { this._visitedCells[i] = []; }
+        },
+        step: function(maze) {
+            var maze = this._maze;
+            var cellStack = this._cellStack;
+            var c = cellStack[cellStack.length - 1];
+
+            if(this._visitedCells[c.row][c.column]) {
+                this.emit("revisited", c);
+            } else {
+                this._visitedCells[c.row][c.column] = true;
+                this.emit("visited", c);
+            }
+
+            if(c.row === maze.endLocation.row && c.column === maze.endLocation.column) {
+                this.emit("solved");
+            } else {
+                var next = this._getNextCell(c.row, c.column);
+                if(next === null) {
+                    this.emit("deadEnd", cellStack.pop());
+                } else {
+                    cellStack.push(next);
+                }
+            }
+        },
+        _getNextCell: function(row, column) {
+            var accessibleNeighbors = this._maze.getAccessibleNeighbors(row, column);
+            var options = accessibleNeighbors.filter(lang.hitch(this, function(neighbor) {
+                return !this._visitedCells[neighbor.row][neighbor.column]
+            }));
+                               
+            return options.length === 0
+                ? null
+                : options[Math.floor(Math.random() * options.length)]
+        }
+
     });
 });
